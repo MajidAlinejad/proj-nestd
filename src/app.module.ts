5171a82d51e1192c372c085d86beb083d4b645ca
majid <alinejad.ui@gmail.com>
2024-04-16 11:20:43 +0330

    add microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NestModule } from './nest/nest.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [NestModule],
+  imports: [NestModule, UserModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
