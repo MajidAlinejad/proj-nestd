811acdc18e8f2823b550eef062c5459ebc56ef23
majid <alinejad.ui@gmail.com>
2024-04-16 11:41:44 +0330

    update 5 files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NestModule } from './nest/nest.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NestModule, UserModule],
+  imports: [NestModule, UserModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
